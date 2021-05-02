0f91c4a05ba2e02fccfe3b75bc9a1bf7005f03b9
Litou-lyh <53361976+Litou-lyh@users.noreply.github.com>
2021-05-02 15:19:01 +0800

    add student.py and main.py, backend procedures in main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,10 +3,44 @@ from student import Student
 import course
 from course import Course, Instructor, Session
 
+'''
+1.LOGIN
+'''
+
 # login ==> stuid & pwd
 
 # stuid -> db -> pwd == pwd
 
-# initialization of Student instance
+# Initialization of Schedule and Student instance
+
+# schedule = Schedule()
+
 # s = Student(stuid, name, school, major, year, tot_credit, studied_course, schedule)
 s = Student('118010154','lyh','SDS','CSE', 3, 90, ['CSC4001'])
+
+'''
+2. ENTER PROFILE PAGE
+'''
+# Can view studied courses here
+
+# View one, initialize one, plus student Grade point 
+
+'''
+3. SEARCH COURSE
+'''
+
+# Query a category of courses
+
+# Initialize these courses together
+# Show basic info of coutses
+
+'''
+4. COURSE PAGE
+'''
+
+# Call Course methods and Schedule Methods
+
+
+'''
+5. 
+'''
